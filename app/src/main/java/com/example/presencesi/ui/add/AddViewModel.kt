e9e6064b606a6bae9a9a01d08df5109ed8e8de42
khawarizmi  azis septian <khawarizmi.azis.ft21@mail.umy.ac.id>
2024-01-11 21:10:49 +0700

    add AddViewModel.kt

--- app/src/main/java/com/example/presencesi/ui/add/AddViewModel.kt
+++ app/src/main/java/com/example/presencesi/ui/add/AddViewModel.kt
@@ -1,2 +1,24 @@
 package com.example.presencesi.ui.add
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.presencesi.data.RepositoryPresensi
+import com.example.presencesi.ui.AddEvent
+import com.example.presencesi.ui.AddUIState
+import com.example.presencesi.ui.toPresensi
+
+class AddViewModel(private val repositoryPresensi: RepositoryPresensi) : ViewModel() {
+
+    var addUIState by mutableStateOf(AddUIState())
+        private set
+
+    fun updateAddUIState(addEvent: AddEvent) {
+        addUIState = AddUIState(addEvent = addEvent)
+    }
+
+    suspend fun addPresensi() {
+        repositoryPresensi.save(addUIState.addEvent.toPresensi())
+    }
+}
