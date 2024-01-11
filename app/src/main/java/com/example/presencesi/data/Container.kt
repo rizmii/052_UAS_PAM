1e958acad1a0e0fce1f46e8835d64a4218634ea5
khawarizmi  azis septian <khawarizmi.azis.ft21@mail.umy.ac.id>
2024-01-11 19:54:52 +0700

    add Presensi.kt

--- app/src/main/java/com/example/presencesi/data/Container.kt
+++ app/src/main/java/com/example/presencesi/data/Container.kt
@@ -1,2 +1,10 @@
 package com.example.presencesi.data
 
+data class Presensi(
+    val NIM : String,
+    val nama : String,
+    val tanggal : String,
+    val keterangan : String
+){
+    constructor(): this("","","","")
+}
--- app/src/main/java/com/example/presencesi/model/Presensi.kt
+++ app/src/main/java/com/example/presencesi/model/Presensi.kt
@@ -1,6 +1,6 @@
 package com.example.presencesi.model
 
-data class Attend(
+data class Presensi(
     val id : String,
     val nama : String,
     val waktu : String
